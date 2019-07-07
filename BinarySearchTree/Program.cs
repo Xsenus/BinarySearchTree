38b9f280be4a42cc4ba89898cf2c7836d7423ad4
Xsenus_Home <ilel@list.ru>
2019-07-07 13:54:37 +0700

    Добавьте файлы проекта.

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,45 @@
+﻿using BinarySearchTree.Model;
+using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = new Tree<int>();
+
+            tree.Add(5);
+            tree.Add(3);
+            tree.Add(7);
+            tree.Add(1);
+            tree.Add(2);
+            tree.Add(8);
+            tree.Add(6);
+            tree.Add(9);
+            tree.Add(4);
+
+            Console.Write("Preorder: ");
+            foreach (var item in tree.Preorder())
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+
+            Console.Write("Postorder: ");
+            foreach (var item in tree.Postorder())
+            {
+                Console.Write($"{item} ");
+            }
+            Console.WriteLine();
+
+            Console.Write("Inorder: ");
+            foreach (var item in tree.Inorder())
+            {
+                Console.Write($"{item} ");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
